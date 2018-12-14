b5e44d227c781adb2deb819c96fb1321a5a5083e
nsyawali12 <nsyawali12@gmail.com>
2018-12-14 10:54:50 +0700

    coba java

--- app/src/main/java/com/example/kojocoffee/kojocoffee_v2/in_dbarang.java
+++
@@ -1,7 +0,0 @@
-package com.example.kojocoffee.kojocoffee_v2;
-
-
-
-public class in_dbarang {
-
-}
---
+++ app/src/main/java/com/example/kojocoffee/kojocoffee_v2/input_dbarang.java
@@ -0,0 +1,26 @@
+package com.example.kojocoffee.kojocoffee_v2;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.view.ViewGroup;
+
+
+public class input_dbarang extends MainActivity{
+
+    RelativeLayout in_dbarang;
+
+    String namaBrg;
+    int hargaBrg,
+
+    EditText in_nmBrg;
+    EditText in_hrgBrg;
+    EditText
+
+
+
+}
