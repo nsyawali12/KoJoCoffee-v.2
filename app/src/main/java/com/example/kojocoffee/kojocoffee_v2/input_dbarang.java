1c7ee5f4165d6507067e607c9c6b6732763f542d
nsyawali12 <nsyawali12@gmail.com>
2018-12-15 01:17:52 +0700

    gak ada error

--- app/src/main/java/com/example/kojocoffee/kojocoffee_v2/input_dbarang.java
+++ app/src/main/java/com/example/kojocoffee/kojocoffee_v2/input_dbarang.java
@@ -1,5 +1,6 @@
 package com.example.kojocoffee.kojocoffee_v2;
 
+import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
@@ -15,11 +16,41 @@ public class input_dbarang extends MainActivity{
     RelativeLayout in_dbarang;
 
     String namaBrg;
-    int hargaBrg,
+    int hargaBrg;
+    int jumBrg;
 
     EditText in_nmBrg;
     EditText in_hrgBrg;
-    EditText
+    EditText in_jumBrg;
+
+    Button but_confBrg;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.in_dbarang);
+
+        in_nmBrg = (EditText) findViewById(R.id.in_nmBrg);
+        in_hrgBrg = (EditText) findViewById(R.id.in_hrgBrg);
+        in_jumBrg = (EditText) findViewById(R.id.in_jumBrg);
+
+        but_confBrg = (Button) findViewById(R.id.but_confBrg);
+        but_confBrg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                namaBrg = in_nmBrg.getText().toString();
+                hargaBrg = Integer.valueOf(in_hrgBrg.getText().toString());
+            }
+        });
+
+
+    }
+
+
+
+
+
 
 
 
