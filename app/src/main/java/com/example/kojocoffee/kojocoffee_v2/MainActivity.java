509866277c76a8cb6f92db2fd6b8adac7fd303f2
lala <annisa.suciati1406@gmail.com>
2018-12-15 19:47:51 +0700

    edit intro

--- app/src/main/java/com/example/kojocoffee/kojocoffee_v2/MainActivity.java
+++ app/src/main/java/com/example/kojocoffee/kojocoffee_v2/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.kojocoffee.kojocoffee_v2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro);
+
+        Button loginBtn = (Button) findViewById(R.id.login);
+        loginBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent startIntent =  new Intent(getApplicationContext(), pilih_akun.class);
+                startActivity(startIntent);
+            }
+        });
+
     }
 
 
