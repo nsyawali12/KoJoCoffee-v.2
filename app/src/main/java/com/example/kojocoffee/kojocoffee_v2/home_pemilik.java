f7a766b0795cb7cd395006a9e172b1c277021858
lala <annisa.suciati1406@gmail.com>
2018-12-15 20:50:05 +0700

    java ONLY show page completed

---
+++ app/src/main/java/com/example/kojocoffee/kojocoffee_v2/home_pemilik.java
@@ -0,0 +1,13 @@
+package com.example.kojocoffee.kojocoffee_v2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class home_pemilik extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_pemilik);
+    }
+}
