40495d4090e81d12844f9a2a830c74c5ca63712d
lala <annisa.suciati1406@gmail.com>
2018-12-15 22:22:54 +0700

    java page move button (akun)

--- app/src/main/java/com/example/kojocoffee/kojocoffee_v2/home_pstand.java
+++ app/src/main/java/com/example/kojocoffee/kojocoffee_v2/home_pstand.java
@@ -1,7 +1,10 @@
 package com.example.kojocoffee.kojocoffee_v2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class home_pstand extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class home_pstand extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_pstand);
+
+        Button in_dstandBtn = (Button) findViewById(R.id.in_dstand);
+        Button up_standBtn = (Button) findViewById(R.id.up_stand);
+
+        in_dstandBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent startIntent =  new Intent(getApplicationContext(), in_dstand.class);
+                startActivity(startIntent);
+            }
+        });
+
+        up_standBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent startIntent =  new Intent(getApplicationContext(), up_stand.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/kojocoffee/kojocoffee_v2/home_supplier.java
+++ app/src/main/java/com/example/kojocoffee/kojocoffee_v2/home_supplier.java
@@ -1,7 +1,10 @@
 package com.example.kojocoffee.kojocoffee_v2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class home_supplier extends AppCompatActivity {
 
@@ -10,5 +13,42 @@ public class home_supplier extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_supplier);
+
+        Button in_barangBtn = (Button) findViewById(R.id.in_dbarang);
+        Button in_bahanBtn = (Button) findViewById(R.id.in_dbahan);
+        Button up_bahanBtn = (Button) findViewById(R.id.up_bahan);
+        Button up_barangBtn = (Button) findViewById(R.id.up_barang);
+
+        in_barangBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent startIntent =  new Intent(getApplicationContext(), in_dbarang.class);
+                startActivity(startIntent);
+            }
+        });
+
+        in_bahanBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent startIntent =  new Intent(getApplicationContext(), in_dbahan.class);
+                startActivity(startIntent);
+            }
+        });
+
+        up_bahanBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent startIntent =  new Intent(getApplicationContext(), up_bahan.class);
+                startActivity(startIntent);
+            }
+        });
+
+        up_barangBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent startIntent =  new Intent(getApplicationContext(), up_barang.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
