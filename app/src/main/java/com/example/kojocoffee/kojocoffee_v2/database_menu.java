d32411dfb4d7cdda11d6ddbc78c0ef6b4f66dd47
lala <annisa.suciati1406@gmail.com>
2018-12-20 03:31:20 +0700

    ALL stand

---
+++ app/src/main/java/com/example/kojocoffee/kojocoffee_v2/database_menu.java
@@ -0,0 +1,31 @@
+package com.example.kojocoffee.kojocoffee_v2;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class database_menu extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "database_menu.db";
+    private static final int DATABASE_VERSION = 1;
+    public database_menu(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        String sql = "create table data_menu(namaMenu text null, hargaMenu integer null);";
+        Log.d("Data", "onCreate: " + sql);
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int arg1, int arg2) {
+
+    }
+}
